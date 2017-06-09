5a5be8981db4268564251c4bab2b2676fb206ee3
Administrator <Administrator@A-201703031206>
2017-06-09 16:12:00 +0800

    1111

---
+++ githubtest/src/text/Ta.java
@@ -0,0 +1,13 @@
+package text;
+
+
+public class Ta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ta ta=new Ta();
+		System.out.println("c");
+		System.out.print("heheda");
+	}
+
+}
